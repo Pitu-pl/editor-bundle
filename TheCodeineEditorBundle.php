78b35883e091f0cbcbac943fd4957766613b8568
Adam Szajuk <adam.szajuk@thecodeine.com>
2015-09-03 11:44:44 +0200

    TUNACMS-4 usunąć wszelkie stylowanie formularzy które są w poszczególnych bundlach

--- TheCodeineEditorBundle.php
+++ TheCodeineEditorBundle.php
@@ -4,16 +4,6 @@ namespace TheCodeine\EditorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-use TheCodeine\EditorBundle\DependencyInjection\Compiler\TwigFormPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 class TheCodeineEditorBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new TwigFormPass());
-    }
-
 }
