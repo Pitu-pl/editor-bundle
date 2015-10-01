04a4ba484b88a0dcdae4cd8be3070da5fcdddc5c
Adam Szajuk <adam.szajuk@thecodeine.com>
2015-10-01 14:47:13 +0200

    run-tests.sh

--- run-server.sh
+++ run-server.sh
@@ -2,6 +2,6 @@
 php Behat/Fixtures/app/console --env=test --no-debug doctrine:schema:drop --force
 php Behat/Fixtures/app/console --env=test --no-debug doctrine:schema:create
 php Behat/Fixtures/app/console --env=test --no-debug assets:install Behat/Fixtures/web/
-php Behat/Fixtures/app/console --env=test --no-debug assetic:dump
+php Behat/Fixtures/app/console assetic:dump
 php Behat/Fixtures/app/console --env=test --no-debug cache:clear
 php Behat/Fixtures/app/console --env=test --no-debug server:run
